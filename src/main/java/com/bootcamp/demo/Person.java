cbaf39e28f5dd622e0560fe49427ef26774cd700
nitzel <lykaparuan@gmail.com>
2022-11-04 14:52:00 +0800

    Added last name property

--- src/main/java/com/bootcamp/demo/Person.java
+++ src/main/java/com/bootcamp/demo/Person.java
@@ -3,18 +3,28 @@ package com.bootcamp.demo;
 public class Person {
 
 	private String firstName;
+	private String lastName;
 
-	public Person(String firstName) {
-		super();
+	public Person(String firstName, String lastName) {
 		this.firstName = firstName;
+		this.lastName = lastName;
 	}
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
-	
-	public void setFirstName() {
+
+	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 	
+
 }
